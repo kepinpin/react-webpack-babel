e52bab9554a4b4dd96cc2672fdbb9ace5b6ecfeb
Kevin Hendrawan <kevin.hendrawan@coderscolony.com>
2018-10-23 18:15:35 +0700

    the HTML webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+	module: {
+	rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {loader: "babel-loader"}
+			},
+			{
+				test: /\.html$/,
+				exclude: /node_modules/,
+				use: {loader: "html-loader"}
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html"
+		})
+	]
 };
