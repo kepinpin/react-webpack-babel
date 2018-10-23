96aad9df3d8eec92fb67ca5724ec63c418b6f816
Kevin Hendrawan <kevin.hendrawan@coderscolony.com>
2018-10-23 16:56:55 +0700

    writing React components

---
+++ src/js/components/container/FormContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Input from "../presentational/Input";
+
+class FormContainer extends Component {
+	constructor() {
+		super();
+		this.state = {seo_title: ""};
+	}
+
+	handleChange(event) {
+		this.setState({[event.target.id]: event.target.value});
+	}
+
+	render() {
+		const {seo_title} = this.state;
+		return (
+			<form id="article-form">
+				<Input
+					text="SEO Title"
+					label="seo_title"
+					type="text"
+					id="seo_title"
+					value={seo_title}
+					handleChange={this.handleChange}
+				/>
+			</form>
+		);
+	}
+}
+export default FormContainer;
